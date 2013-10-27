244e7994fc6bcbec79e5912c7c8411e6a748236a
Daniel Weller <ravensfan55222@aol.com>
2013-10-26 22:56:43 -0400

    Home and Contact are working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
-describe "Static pages" do
+  describe "Static pages" do
 
-  describe "Home page" do
+    describe "Home page" do
 
-    it "should have the content 'Everything Baseball'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Everything Baseball')
+      it "should have the content 'Everything Baseball'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Everything Baseball')
+      end
+    end
+
+    describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
     end
   end
 end
