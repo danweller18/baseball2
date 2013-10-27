b9d4ecc4b21a65079a07d3282d1c18cb72b92969
Daniel Weller <ravensfan55222@aol.com>
2013-10-27 17:23:43 -0400

    Home + Contact + About working pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,47 +2,27 @@ require 'spec_helper'
 
   describe "Static pages" do
 
+    subject { page }
+
     describe "Home page" do
+      before { visit root_path }
 
-      it "should have the content 'Everything Baseball'" do
-        visit '/static_pages/home'
-        expect(page).to have_content('Everything Baseball')
-      end
-   
-      it "should have the base title" do
-        visit '/static_pages/home'
-        expect(page).to have_title("Everything Baseball")
-      end
-
-      it "should not have a custom page title" do
-	visit '/static_pages/home'
-	expect(page).not_to have_title('| Home')
-      end
+      it { should have_content('Everything Baseball') }
+      it { should have_title(full_title('')) }
+      it { should_not have_title('| Home') }
     end
 
     describe "Contact page" do
+      before { visit contact_path }
 
-      it "should have the content 'Contact Us'" do
-        visit '/static_pages/contact'
-        expect(page).to have_content('Contact Us')
-      end
-
-      it "should have the title 'Contact Us'" do
-        visit '/static_pages/contact'
-        expect(page).to have_title("Everything Baseball | Contact Us")
-      end
+      it { should have_content('Contact') }
+      it { should have_title(full_title('Contact Us')) }
     end
 
     describe "About page" do
+      before { visit about_path }
 
-      it "should have the content 'About Us'" do
-        visit '/static_pages/about'
-        expect(page).to have_content('About Us')
-      end
-      
-      it "should have the title 'About Us'" do
-        visit '/static_pages/about'
-        expect(page).to have_title("Everything Baseball | About Us")
-      end
-     end
+      it { should have_content('About') }
+      it { should have_title(full_title('About Us')) }
+    end
   end
