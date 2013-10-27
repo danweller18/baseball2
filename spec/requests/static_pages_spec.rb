70ca6124769b4a6f9fd026e62b2407cc188a64aa
Daniel Weller <ravensfan55222@aol.com>
2013-10-26 23:33:47 -0400

    Shortening title code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ require 'spec_helper'
         visit '/static_pages/home'
         expect(page).to have_content('Everything Baseball')
       end
+   
+      it "should have the title 'Home'" do
+        visit '/static_pages/home'
+        expect(page).to have_title("Everything Baseball | Home")
+      end
     end
 
     describe "Contact page" do
@@ -15,6 +20,11 @@ require 'spec_helper'
       it "should have the content 'Contact Us'" do
         visit '/static_pages/contact'
         expect(page).to have_content('Contact Us')
+      end
+
+      it "should have the title 'Contact Us'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Everything Baseball | Contact Us")
       end
     end
 
@@ -24,5 +34,10 @@ require 'spec_helper'
         visit '/static_pages/about'
         expect(page).to have_content('About Us')
       end
-    end
+      
+      it "should have the title 'About Us'" do
+        visit '/static_pages/about'
+        expect(page).to have_title("Everything Baseball | About Us")
+      end
+     end
   end
